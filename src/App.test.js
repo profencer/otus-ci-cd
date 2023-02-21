444e64acb15c9009388570da7cc0ae2657de890d
Ars Wysocki <arswysocki@gmail.com>
2023-02-21 20:57:55 +0300

    up

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import App, {id} from './App';
+import App, { id } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
+
 test('id function works as expected', () => {
   expect(id(1)).toEqual(1);
-})
+});
